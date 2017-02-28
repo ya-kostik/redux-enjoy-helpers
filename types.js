2b51b8d89bd8fed49758b99ecdb7562fafc6dcd0
Constantin Tsukanov <c@indotech.ru>
2017-02-28 17:49:58 +0300

    Types with babel

--- types.js
+++ types.js
@@ -1,3 +1,14 @@
 "use strict";
 
-function create() {}
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function create(prefix, types) {
+  var TYPES = {};
+  types.forEach(function (type) {
+    TYPES[type] = prefix + type;
+  });
+  return TYPES;
+}
+
+exports.default = create;
